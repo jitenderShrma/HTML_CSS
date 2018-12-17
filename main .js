06c2d3026bea3aa2ed0ed1438101edefcfed2dd9
jitenderShrma <jitenderkanti020@gmail.com>
2018-12-17 08:24:44 +0530

    changed

---
+++ main .js
@@ -0,0 +1,9 @@
+let modelBtn = document.getElementById('cross');
+let outer = document.getElementById('outer');
+let model = document.getElementById('model');
+
+modelBtn.addEventListener('click', function(){
+  model.style.display = 'none';
+  outer.style.background = '#ffffff'
+});
+
